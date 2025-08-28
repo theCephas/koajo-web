034bb77f347362ccc8700a4a799b1c8d4b89f48d
Israel-Beni <bendegbe72@gmail.com>
2025-08-28 22:31:12 +0200

    feat(utils): add getCurrencySymbol function to return symbols for USD, GBP, and EUR

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,3 +1,15 @@
+import { Currency } from '@/types';
 import { v4 as uuidv4 } from 'uuid';
 
 export const generateUid = () => uuidv4();
+
+export const getCurrencySymbol = (currency: Currency) => {
+  switch (currency) {
+    case "USD":
+      return "$";
+    case "GBP":
+      return "£";
+    case "EUR":
+      return "€";
+  }
+};
