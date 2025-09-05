a4528bb8c6476bb911efff45a4e10bb902a19093
Israel-Beni <bendegbe72@gmail.com>
2025-09-05 03:16:35 +0200

    feat(utils): export new components including Label, Input, Field, PasswordField, and ProgressTracker for enhanced form functionality and user experience

--- components/utils/index.ts
+++ components/utils/index.ts
@@ -1,5 +1,10 @@
 export { default as MyImage } from './my-image';
 export { Button } from './button';
+export { Label } from './label';
+export { Input } from './input';
+export { Field } from './field';
+export { PasswordField } from './password-field';
+export { ProgressTracker, OnboardingIcons } from './progress-tracker';
 export type { MyImageProps, ResponsiveImageConfig } from './my-image';
 export { default as PlanCard } from './card-plan'; 
 export { default as MyCldImage } from './my-cld-image';
