639e0f8d55aa8e7063b1afa9455be0f282b20aaf
Israel-Beni <bendegbe72@gmail.com>
2025-09-05 03:19:32 +0200

    feat(dashboard): update dashboard layout with new Pod components for enhanced overview

--- app/(admin)/dashboard/(overview)/page.tsx
+++ app/(admin)/dashboard/(overview)/page.tsx
@@ -1,13 +1,18 @@
 import styles from "./DashboardV2Page.module.sass";
 import Layout from "@/components2/usefull/Layout";
 import Navigation from "@/components2/usefull/Navigation";
-import TotalBalance from "@/components2/usefull/TotalBalance";
+import PodInfo from "@/components/admin/pod-info";  
+import PodGoals from "@/components/admin/pod-goals";
+import PodMembers from "@/components/admin/pod-members";
+import Achievements from "@/components/admin/achievements";
 import PocketPlans from "@/components2/usefull/PocketPlans";
 import RecentActivity from "@/components2/usefull/RecentActivity";
 import Currency from "@/components2/usefull/Currency";
 import ExpenseCategory from "@/components2/usefull/ExpenseCategory";
-import IncomeAnalysis from "@/components2/usefull/IncomeAnalysis";
+import BalanceInfo from "@/components/admin/blance-info";
 import ExpenseAnalysis from "@/components2/usefull/ExpenseAnalysis";
+import CycleDuration from "@/components/admin/cycle-duration";
+
 
 import { plans } from "@/mocks/plans";
 import { activity } from "@/mocks/activity";
@@ -25,15 +30,17 @@ const Dashboard = () => {
     >
       <div className={styles.row}>
         <div className={styles.col}>
-          <TotalBalance />
+          <PodInfo />
           <RecentActivity viewItems={5} items={activity} />
         </div>
         <div className={styles.col}>
-          {/* <IncomeAnalysis items={incomeAnalysis} expense="$2.172" row />
-           <ExpenseAnalysis items={expenseAnalysis} expense="$1.456" row />
-         <ExpenseCategory items={expenseCategory} /> */}
+          <BalanceInfo />
+          <CycleDuration />
+          <PodGoals />
         </div>
         <div className={styles.col}>
+          <PodMembers />
+          <Achievements />
           <div className={styles.card}>
             {/* <PocketPlans items={plans} />  */}
           </div>
