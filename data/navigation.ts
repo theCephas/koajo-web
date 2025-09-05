4d2ebac3cc92ac3d8a2cb4c814100d8dd8f64b4a
Israel-Beni <bendegbe72@gmail.com>
2025-09-05 03:15:08 +0200

    feat(navigation): add "Get Started" menu item; feat(users): create users data file; refactor(transactions): update transaction details and remove unused captions

--- data/navigation.ts
+++ data/navigation.ts
@@ -4,5 +4,6 @@ export const menuItems = [
   { label: "Career", href: "/career" },
   { label: "FAQ", href: "/faq" },
   { label: "Blog & Press", href: "/blog" },
+  { label: "Get Started", href: "/get-started" },
   { label: "Login", href: "/auth/login" },
 ];
---
+++ data/users.ts
@@ -0,0 +1,9 @@
+const users = [
+  {
+    id: 1,
+    email: "test@test.com",
+    password: "Test@123&",
+  },
+];
+
+export default users;
