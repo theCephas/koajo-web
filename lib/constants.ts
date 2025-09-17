186e210724b50bd68cf76a29285156dd8639a965
Israel-Beni <bendegbe72@gmail.com>
2025-09-17 17:22:50 +0200

    feat(constants): add form field validation patterns and messages for password, email, and phone number

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,42 @@
 import { HTMLTag } from "@/types";
 export const REM_BASE = 16;
 
-const HEADINGS: HTMLTag[] = ["h1", "h2", "h3", "h4", "h5", "h6"];
+export const HEADINGS: HTMLTag[] = ["h1", "h2", "h3", "h4", "h5", "h6"];
+
+export const FORM_FIELDS_PATTERNS = {
+  PASSWORD: {
+    MIN_LENGTH: 10,
+    ALL: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9])/,
+    LOWERCASE: /[a-z]/,
+    UPPERCASE: /[A-Z]/,
+    NUMBER: /[0-9]/,
+    SPECIAL: /[^A-Za-z0-9]/,
+  },
+
+  EMAIL: /\S+@\S+\.\S+/,
+
+  PHONE_NUMBER: /^\+[1-9](?:[ -]?\d){1,14}$/, // E.164 with optional space or dash separators
+};
+
+export const FORM_FIELDS_MESSAGES = {
+  PASSWORD: {
+    ALL: "Password must contain uppercase, lowercase, number, and special characters",
+    LOWERCASE: "Password must contain a lowercase character",
+    UPPERCASE: "Password must contain a uppercase character",
+    NUMBER: "Password must contain a number",
+    SPECIAL: "Password must contain a special character",
+    MIN_LENGTH: "Password must be at least 10 characters",
+    REPEAT: "Passwords do not match",
+    REQUIRED: "Password is required",
+  },
+
+  EMAIL: {
+    PATTERN: "Please enter a valid email address",
+    REQUIRED: "Email is required",
+  },
+
+  PHONE_NUMBER: {
+    PATTERN: "Please enter a valid phone number",
+    REQUIRED: "Phone number is required",
+  },
+};
