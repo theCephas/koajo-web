fe9f631b1ec229514fe6be3a84882b8fe7db0344
Israel-Beni <bendegbe72@gmail.com>
2025-09-22 04:02:03 +0200

    refactor(register-layout): update layout structure and styling for improved responsiveness and visual consistency

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -9,16 +9,16 @@ interface RegisterLayoutProps {
 
 export default function RegisterLayout({ children }: RegisterLayoutProps) {
   return (
-    <div className="bg-gray w-full h-screen min-h-[calc(700rem/16)]">
+    <div className="bg-gray w-full h-dvh">
       {/* Background */}
-      <div className="absolute inset-0 flex gap-10 h-full p-page-auth-offset-horizontal">
-        <div className="w-[calc((428/1396)*100%)] bg-[image:linear-gradient(180deg,#469DA3_0%,#1C2634_100%)] rounded-2xl" />
-      </div>
+      {/* <div className="absolute inset-0 flex gap-10 h-full p-page-auth-offset-horizontal"> */}
+        {/* <div className="w-[calc((428/1396)*100%)] bg-[image:linear-gradient(180deg,#469DA3_0%,#1C2634_100%)] rounded-2xl" /> */}
+      {/* </div> */}
 
       {/* Left Column - Branding */}
-      <div className="page_auth_container flex gap-10 h-full">
-        <div className="lg:flex lg:w-[calc((428/1396)*100%)] relative h-full justify-end">
-          <div className="relative flex flex-col gap-16 px-8 py-6">
+      <div className="page_auth_container flex gap-10 w-full h-full">
+        <div className="lg:flex lg:w-full lg:max-w-[calc((428/1396)*100%)] sticky left-0 top-0 h-full justify-end bg-[image:linear-gradient(180deg,#469DA3_0%,#1C2634_100%)] rounded-2xl">
+          <div className="relative flex flex-col gap-16 px-8 py-6 h-full">
             {/* Logo */}
             <Link href="/" className="flex items-center">
               <Image
@@ -35,12 +35,14 @@ export default function RegisterLayout({ children }: RegisterLayoutProps) {
           </div>
         </div>
 
-        {/* Right Column - Auth Forms */}
-        <div className="w-full grow flex items-center justify-center relative h-full">
-          {children}
+        {/* Right Column  Forms */}
+        <div className="w-full grow flex flex-col h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent">
+          <div className="flex-1 flex items-center justify-center">
+            {children}
+          </div>
 
-          {/* Copyright */}
-          <div className="text-center absolute bottom-0 mt-8 text-sm text-secondary-300 gap-3 flex">
+          {/* Copyright*/}
+          <div className="text-center mt-8 mb-6 text-sm text-secondary-300 gap-3 flex justify-center">
             © Koajo. All rights reserved.
             <Link href="/legals/privacy-policy" className="text-tertiary-100 hover:text-tertiary-100/80">Privacy Policy</Link>
             |
