3b1f02aec6d5d457374ce90676d957b3f4ce8948
Israel-Beni <bendegbe72@gmail.com>
2025-10-18 00:11:58 +0200

    feat(next.config): add redirects for dashboard, register, settings, auth, and help-center paths to enhance routing

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,36 @@ const nextConfig: NextConfig = {
       use: ["@svgr/webpack"]
     });
     return config;
-  }
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/dashboard/:path*',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/register/:path*',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/settings/:path*',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/auth/:path*',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/help-center/:path*',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default withNextVideo(nextConfig, { folder: '/public/media/videos' });
