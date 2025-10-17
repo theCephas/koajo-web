3ea679dff2c6c944b3df317e371ae79a7fa47ac0
Israel-Beni <bendegbe72@gmail.com>
2025-10-17 17:37:38 +0200

    refactor(steps): enhance progress bar functionality and improve ScrollTrigger integration

--- components/main/steps.tsx
+++ components/main/steps.tsx
@@ -1,30 +1,38 @@
 "use client";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
 import cn from "clsx";
 import Image from "next/image";
 import { MyImage } from "../utils";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Steps() {
   const [activeIdx, setActiveIdx] = useState(0);
+  const [progressMaxHeight, setProgressMaxHeight] = useState(0);
   const stepRefs = useRef<(HTMLDivElement | null)[]>([]);
 
   const handleStepClick = (idx: number) => {
     setActiveIdx(idx);
   };
 
+  const progressHeight = useMemo(() => {
+    if (activeIdx === 0) return "2%";
+    return activeIdx === steps.length - 1 ? "100%" : `${((activeIdx + 1) * 100) / steps.length}%`;
+  }, [activeIdx]);
+
   useEffect(() => {
-    // Register ScrollTrigger plugin
-    gsap.registerPlugin(ScrollTrigger);
+    const scrollerEl = document.getElementById("steps-info-container");
 
     // Create ScrollTrigger for each step
     stepRefs.current.forEach((ref, idx) => {
       if (ref) {
         ScrollTrigger.create({
           trigger: ref,
+          scroller: scrollerEl || undefined,
           start: "top 50%",
-          end: "bottom 60%",
+          end: "bottom 52%",
           onEnter: () => handleStepClick(idx),
           onEnterBack: () => handleStepClick(idx),
           markers: true,
@@ -50,6 +58,13 @@ export default function Steps() {
     };
   }, []);
 
+  useEffect(() => {
+    const stepTextContainers = document.querySelectorAll(".step-text-container");
+    const maxHeight = Array.from(stepTextContainers).reduce((acc, container) =>  acc + container.clientHeight, 0);
+    setProgressMaxHeight(maxHeight);
+    console.log("maxHeight", maxHeight);
+  }, []);
+
   return (
     <section className="w-full bg-[image:linear-gradient(250deg,#000_1.2%,#032425_43.67%,#000_96.25%)]">
       <div className="page_container py-15 relative">
@@ -67,7 +82,8 @@ export default function Steps() {
         {/* headings */}
         <div className="flex flex-col gap-5 md:gap-6 items-center text-center mb-10 lg:mb-15 max-w-[calc(940rem/16)] mx-auto">
           <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold text-white">
-          See How We Are  <span className="text-highlight">Empowering Millions...</span>
+            See How We Are{" "}
+            <span className="text-highlight">Empowering Millions...</span>
           </h2>
           <p className="text-sm md:text-base lg:text-lg text-gray max-w-200 mx-auto">
             Koajo modernizes the rotating savings model to help people save
@@ -97,10 +113,16 @@ export default function Steps() {
               />
             ))}
           </div>
-          <div className="relative flex flex-col gap-7.5 lg:gap-50 overflow-auto lg:h-[calc(677rem/16)] scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent hover:scrollbar-thumb-gray-300">
+          <div
+            className="relative flex flex-col overflow-auto lg:h-[calc(677rem/16)] scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-transparent hover:scrollbar-thumb-gray-300"
+            id="steps-info-container"
+          >
             {/* Progress Bar */}
-            <div className="absolute z-0 top-0 left-4.5 w-[1px] h-full bg-gray-100/25">
-              <div className="bg-white h-[2%]" style={{ height: `${(activeIdx + 1) * 100 / steps.length}%` }} />
+            <div className="absolute z-0 top-0 left-4.5 w-[1px] h-full bg-gray-100/25" style={{ height: progressMaxHeight > 0 ? `${progressMaxHeight}px` : "100%" }}>
+              <div
+                className="bg-white h-[2%]"
+                style={{ height: progressHeight }}
+              />
             </div>
 
             {/* steps */}
@@ -113,9 +135,10 @@ export default function Steps() {
                 className="flex gap-14"
               >
                 <div className="relative z-10 size-fit px-2 py-1 bg-white text-black text-md font-medium  rounded-[0.625rem]">
-                  {idx < 8 && "0"}{idx + 1}
+                  {idx < 8 && "0"}
+                  {idx + 1}
                 </div>
-                <div className="flex flex-col gap-4 lg:gap-6">
+                <div className={cn("step-text-container flex flex-col gap-4 lg:gap-6 pb-7.5 lg:pb-50", idx === steps.length - 1 && "pb-0")}>
                   <h3 className="text-2lg md:text-xl lg:text-2xl font-semibold text-transparent bg-clip-text w-full bg-[image:linear-gradient(90deg,#5A606C_0%,#FFFFFF_50%,#5A606C_100%)]">
                     {step.title}
                   </h3>
