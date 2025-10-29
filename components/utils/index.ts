f66354adf527a484f6bff3a4001a5c687bf6a043
Israel-Beni <bendegbe72@gmail.com>
2025-10-29 14:10:59 +0100

    refactor(utils): update Modal export to include ModalProps type

--- components/utils/index.ts
+++ components/utils/index.ts
@@ -11,4 +11,4 @@ export { default as PlanCard } from './card-plan';
 export { default as MyCldImage } from './my-cld-image';
 export { default as MyResponsivePie } from './my-responsive-pie';
 export { default as Donot } from "./donot"
-export { default as Modal } from "./modal"
+export {  Modal, type ModalProps } from "./modal"
