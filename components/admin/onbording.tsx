393191b47dbbd81c093debe45608068439158d23
Israel-Beni <bendegbe72@gmail.com>
2025-10-31 17:46:37 +0100

    feat(onboarding): enable PodFormFilling component in onboarding flow for dynamic step rendering

--- components/admin/onbording.tsx
+++ components/admin/onbording.tsx
@@ -3,6 +3,7 @@ import { Modal } from "@/components/utils";
 import PodSelection from "./pod-plan-selection";
 import { useOnboarding } from "@/lib/provider-onboarding";
 import PodGoalSetting from "@/components/admin/pod-goal-setting";
+import PodFormFilling from "@/components/admin/pod-form";
 
 export default function Onbording({ children }: { children: React.ReactNode }) {
   const { visible, close, step } = useOnboarding(); 
@@ -12,8 +13,8 @@ export default function Onbording({ children }: { children: React.ReactNode }) {
       <Modal visible={visible} onClose={close}>
         {step === "pod_plan_selection" && <PodSelection />}
         {step === "pod_goal_setting" && <PodGoalSetting />}
-        {/* {step === "pod_form_filling" && <PodFormFilling />}
-        {step === "pod_onboarding_complete" && <PodOnboardingComplete />} */}
+         {step === "pod_form_filling" && <PodFormFilling />}
+       {/* {step === "pod_onboarding_complete" && <PodOnboardingComplete />} */}
       </Modal>
     </>
   );
