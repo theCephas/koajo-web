e048302d49d03a6cb16a2e9fdce1babe8ecafe8c
Israel-Beni <bendegbe72@gmail.com>
2025-11-07 08:52:12 +0100

    feat(balance-info): enhance BalanceInfo component to accept inforType prop for conditional rendering of contributions and payouts

--- components/admin/blance-info.tsx
+++ components/admin/blance-info.tsx
@@ -5,7 +5,7 @@ import Tooltip from "@/components2/usefull/Tooltip";
 import { useDashboard } from "@/lib/provider-dashboard";
 import LockedOverlay from "@/components/admin/locked-overlay";
 
-export default function BalanceInfo() {
+export default function BalanceInfo({ inforType = "contribution-and-payout" }: { inforType?: "contribution" | "payout" | "contribution-and-payout" }) {
   const { emailVerified } = useDashboard();
   const isLocked = !emailVerified;
 
@@ -13,7 +13,7 @@ export default function BalanceInfo() {
     <div className="relative">
       <div className={cn("mt-6 p-6 bg-white rounded-2xl")}>
         <div className="flex items-center justify-between gap-4">
-          <div className="flex flex-col gap-4">
+          { inforType !== "payout" && <div className="flex flex-col gap-4">
             <div className="flex items-center text-md font-semibold">
               <span>Total Contribution</span>
               <Tooltip content="Total amount you contributed into this pod since joining it" />
@@ -25,10 +25,12 @@ export default function BalanceInfo() {
               No payout date yet
             </p>
           </div>
+          }
+          {inforType === "contribution-and-payout" && (
+            <div className="w-px self-stretch bg-gray-200" />
+          )}
 
-          <div className="w-px self-stretch bg-gray-200" />
-
-          <div className="flex flex-col gap-4">
+          { inforType !== "contribution" && <div className="flex flex-col gap-4">
             <div className="flex items-center text-md font-semibold">
               <span>Total Payout</span>
               <Tooltip content="Total amount you will receive from this pod" />
@@ -38,9 +40,10 @@ export default function BalanceInfo() {
               Payment less 2.5% transaction fee
             </p>
           </div>
+          }
         </div>
       </div>
-      {isLocked && <LockedOverlay />}
+       <LockedOverlay />
     </div>
   );
 }
