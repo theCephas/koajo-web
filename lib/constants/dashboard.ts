31d39ce26e2e5e12b306382fbdb9b8a84b0da511
Israel-Beni <bendegbe72@gmail.com>
2025-11-07 08:46:20 +0100

    feat(dashboard): refactor registration stages and add dashboard breadcrumbs for improved navigation

--- lib/constants/dashboard.ts
+++ lib/constants/dashboard.ts
@@ -1,14 +1,19 @@
-export type RegistrationStage = "none" | "registered" | "kyc_document_complete" | "kyc_id_number_complete" | "email_verified" | "bank_connected";
+export type RegistrationStage =
+  | "none"
+  | "registered"
+  | "kyc_document_complete"
+  | "kyc_id_number_complete"
+  | "email_verified"
+  | "bank_connected";
 
-export const REGISTRATION_STAGE: Record<string, RegistrationStage> = 
-{
+export const REGISTRATION_STAGE: Record<string, RegistrationStage> = {
   NONE: "none",
   REGISTERED: "registered",
   KYC_DOCUMENT_COMPLETE: "kyc_document_complete",
   KYC_ID_NUMBER_COMPLETE: "kyc_id_number_complete",
   EMAIL_VERIFIED: "email_verified",
   BANK_CONNECTED: "bank_connected",
-}
+};
 
 export const REGISTRATION_STAGE_MAP = {
   NONE: "/register",
@@ -17,4 +22,69 @@ export const REGISTRATION_STAGE_MAP = {
   KYC_ID_NUMBER_COMPLETE: "/register/verify-email?stage=email_verified",
   EMAIL_VERIFIED: "/register/kyc?stage=bank_connected",
   BANK_CONNECTED: "/register/complete",
-}
+};
+
+export const DASHBOARD_BREADCRUMBS = {
+  OVERVIEW: [
+    {
+      title: "Dashboard",
+    },
+    {
+      title: "Overview",
+      url: "/dashboard",
+    },
+  ],
+  POD_INFO_AND_TRANSACTIONS: [
+    {
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+    {
+      title: "Transactions",
+      url: "/dashboard/pod-info-and-transactions",
+    },
+  ],
+  PERSONAL_INFORMATION: [
+    {
+      title: "Settings",
+    },
+    {
+      title: "Personal Information",
+      url: "/settings",
+    },
+  ],
+  PREFERENCES: [
+    {
+      title: "Settings",
+    },
+    {
+      title: "Preferences",
+      url: "/settings/preferences",
+    },
+  ],
+  SECURITY: [
+    {
+      title: "Settings",
+    },
+    {
+      title: "Security",
+      url: "/settings/security",
+    },
+  ],
+  SUBSCRIPTIONS: [
+    {
+      title: "Settings",
+    },
+    {
+      title: "Subscriptions",
+      url: "/settings/subscriptions",
+    },
+  ],
+  HELP_AND_CENTER: [
+    {
+      title: "Help & Center",
+      url: "/help-center",
+    },
+  ],
+};
+
